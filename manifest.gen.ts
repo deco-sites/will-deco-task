01f34743ec51f4c84d6e9279f1139f538eb2e4d0
WilliansCMathias_Social <w.mathias@socialsa.com>
2024-05-30 23:27:36 -0300

    feat: aula 3

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -55,8 +55,9 @@ import * as $$$$$$43 from "./sections/Product/ShelfWithImage.tsx";
 import * as $$$$$$44 from "./sections/Product/Wishlist.tsx";
 import * as $$$$$$45 from "./sections/Social/InstagramPosts.tsx";
 import * as $$$$$$46 from "./sections/Social/WhatsApp.tsx";
-import * as $$$$$$47 from "./sections/TextoTeste.tsx";
-import * as $$$$$$48 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$47 from "./sections/Temperature/Temperature.tsx";
+import * as $$$$$$48 from "./sections/TextoTeste.tsx";
+import * as $$$$$$49 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "loaders": {
@@ -120,8 +121,9 @@ const manifest = {
     "deco-sites/will-deco-task/sections/Product/Wishlist.tsx": $$$$$$44,
     "deco-sites/will-deco-task/sections/Social/InstagramPosts.tsx": $$$$$$45,
     "deco-sites/will-deco-task/sections/Social/WhatsApp.tsx": $$$$$$46,
-    "deco-sites/will-deco-task/sections/TextoTeste.tsx": $$$$$$47,
-    "deco-sites/will-deco-task/sections/Theme/Theme.tsx": $$$$$$48,
+    "deco-sites/will-deco-task/sections/Temperature/Temperature.tsx": $$$$$$47,
+    "deco-sites/will-deco-task/sections/TextoTeste.tsx": $$$$$$48,
+    "deco-sites/will-deco-task/sections/Theme/Theme.tsx": $$$$$$49,
   },
   "apps": {
     "deco-sites/will-deco-task/apps/decohub.ts": $$$$$$$$$$$0,
