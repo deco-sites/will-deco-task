c5b78d7f54f9ca663476acf642183bdc43bb22b4
WilliansCMathias_Social <w.mathias@socialsa.com>
2024-06-05 01:25:30 -0300

    arrumando o loader

--- sdk/analytics.tsx
+++ sdk/analytics.tsx
@@ -1,6 +1,16 @@
-import type { AnalyticsEvent } from "apps/commerce/types.ts";
+import type { AnalyticsEvent, IEvent } from "apps/commerce/types.ts";
 
-export const sendEvent = <E extends AnalyticsEvent>(event: E) => {
+interface ScoreParams {
+  score: number;
+  level?: number;
+  character?: string;
+}
+
+interface ScoreEvent extends IEvent<ScoreParams> {
+  name: "post_score"
+}
+
+export const sendEvent = <E extends AnalyticsEvent | ScoreEvent>(event: E) => {
   console.log(JSON.stringify(event, null, 2));
   globalThis.window.DECO.events.dispatch(event);
 };
